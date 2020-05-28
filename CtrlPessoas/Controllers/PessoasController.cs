5bc176822f0b55290d5dbed9b96e08ad410910e9
William Tomé <william.tomenh@gmail.com>
2020-05-27 22:28:22 -0300

    Alterada ação de post na API.

---
+++ CtrlPessoas/Controllers/PessoasController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CtrlPessoas.Models;
+
+namespace CtrlPessoas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoasController : ControllerBase
+    {
+        private readonly CtrlPessoaContext _context;
+
+        public PessoasController(CtrlPessoaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pessoas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoas()
+        {
+            return await _context.Pessoas.ToListAsync();
+        }
+
+        // GET: api/Pessoas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pessoa>> GetPessoa(long id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return pessoa;
+        }
+
+        // PUT: api/Pessoas/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPessoa(long id, Pessoa pessoa)
+        {
+            if (id != pessoa.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pessoa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PessoaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pessoas
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Pessoa>> PostPessoa(Pessoa pessoa)
+        {
+            _context.Pessoas.Add(pessoa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPessoa), new { id = pessoa.Id }, pessoa);
+        }
+
+        // DELETE: api/Pessoas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Pessoa>> DeletePessoa(long id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pessoas.Remove(pessoa);
+            await _context.SaveChangesAsync();
+
+            return pessoa;
+        }
+
+        private bool PessoaExists(long id)
+        {
+            return _context.Pessoas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CtrlPessoas/Models/CtrlPessoaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CtrlPessoas.Models
+{
+    public class CtrlPessoaContext: DbContext
+    {
+        public CtrlPessoaContext(DbContextOptions<CtrlPessoaContext> options):base(options)
+        { }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
