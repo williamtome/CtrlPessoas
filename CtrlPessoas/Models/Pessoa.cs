9fbfd2db2d7257316a7207e4e4a4bf708a53032e
William Tomé <william.tomenh@gmail.com>
2020-05-27 21:06:19 -0300

    Adicionar arquivos de projeto.

---
+++ CtrlPessoas/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CtrlPessoas.Models
+{
+    public class Pessoa
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public bool Ativado { get; set; }
+    }
+}
